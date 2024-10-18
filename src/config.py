635a2854d320a0f2a8afc5294880c2d8f800ae2e
naokii5 <ik.11.naoki@gmail.com>
2024-10-18 14:08:33 +0000

    feat: Add Supabase configuration and query for habits table

---
+++ src/config.py
@@ -0,0 +1,16 @@
+import os
+from supabase import create_client, Client
+from supabase.client import ClientOptions
+from dotenv import load_dotenv
+load_dotenv(verbose=True)
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+
+supabase_client: Client = create_client(url, key,
+  options=ClientOptions(
+    postgrest_client_timeout=10,
+    storage_client_timeout=10,
+    schema="public",
+  ))
+

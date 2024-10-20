1bd0e376b4f2c6866492645f1a434617835919c8
naokii5 <ik.11.naoki@gmail.com>
2024-10-20 07:27:15 +0000

    refactor: Update Supabase client configuration and add CORS origins

--- src/config.py
+++ src/config.py
@@ -7,10 +7,15 @@ load_dotenv(verbose=True)
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
 
-supabase_client: Client = create_client(url, key,
-  options=ClientOptions(
-    postgrest_client_timeout=10,
-    storage_client_timeout=10,
-    schema="public",
-  ))
+supabase: Client = create_client(url, key,
+                                 options=ClientOptions(
+                                     postgrest_client_timeout=10,
+                                     storage_client_timeout=10,
+                                     schema="public",
+                                 ))
 
+# CORSの設定（フロントエンドからのリクエストを許可）
+origins = [
+    "http://localhost:5173",  # フロントエンドのアドレス
+    "http://127.0.0.1:5173",
+]

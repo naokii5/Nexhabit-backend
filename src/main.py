89c5c1b4c3b9d5b0bd953d4d176d0be58ae0ed25
naokii5 <ik.11.naoki@gmail.com>
2024-10-20 10:47:55 +0000

    user, login, logout, sign up,profile

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI, HTTPException, Response, Request
+from fastapi import FastAPI, HTTPException, Response, Request, Depends, Cookie
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-from config import origins, supabase
+from src.config import origins, supabase
 app = FastAPI()
 
 
@@ -68,8 +68,10 @@ def login(user: User, response: Response):
         value=access_token,
         httponly=True,
         secure=True,
-        samesite="Lax"
+        samesite=None
     )
+    print(response.headers)
+
     return {"message": "ログインに成功しました。"}
 
 
@@ -81,11 +83,42 @@ def logout(response: Response):
 
 def get_current_user(request: Request):
     access_token = request.cookies.get("access_token")
+    print(f"Cookies: {request.cookies}")
+    print(f"access_token: {access_token}")
+
     if not access_token:
+        print("No access token")
         raise HTTPException(status_code=401, detail="認証情報がありません。")
 
+    # JWT トークンを明示的に渡してユーザー情報を取得
     user_response = supabase.auth.get_user(access_token)
-    if user_response.get("error"):
+    if user_response.error:
+        print(user_response.error)
         raise HTTPException(status_code=401, detail="無効なトークンです。")
 
-    return user_response["data"]["user"]
+    return user_response.user
+
+
+def get_token_from_cookie(access_token: str | None = Cookie(default=None)):
+    if (access_token is None):
+        raise HTTPException(
+            status_code=401,
+            detail="Not authenticated",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return access_token
+
+
+@app.get("/profile")
+def read_profile(jwt=Depends(get_token_from_cookie)):
+    try:
+        user_response = supabase.auth.get_user(jwt)
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=400, detail=str(e))
+    current_user = user_response.user
+
+    return {
+        "email": current_user.email,
+        "user_metadata": current_user.user_metadata,
+    }

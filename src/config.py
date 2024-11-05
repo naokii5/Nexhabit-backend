4e322120d0539d97f6ee52b29b874afcd537cff6
naokii5 <ik.11.naoki@gmail.com>
2024-11-05 07:14:04 +0000

    frontend_urlの環境変数化

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ load_dotenv(verbose=True)
 
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
-
+frontend_url: str = os.environ.get("FRONTEND_URL")
 supabase: Client = create_client(url, key,
                                  options=ClientOptions(
                                      postgrest_client_timeout=10,
@@ -15,7 +15,4 @@ supabase: Client = create_client(url, key,
                                  ))
 
 # CORSの設定（フロントエンドからのリクエストを許可）
-origins = [
-    "http://localhost:5173",  # フロントエンドのアドレス
-    "http://127.0.0.1:5173",
-]
+origins = [frontend_url]
